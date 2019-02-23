c588939ff0d6b92f1ae146a095f51c2afb561afc
Daniel Estrada <isc.daniel.estrada@gmail.com>
2019-02-23 12:58:10 -0600

    .

---
+++ app/src/main/java/le/petit/prince/layout/Login.java
@@ -0,0 +1,15 @@
+package le.petit.prince.layout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import le.petit.prince.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
